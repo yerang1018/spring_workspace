0038846f0a8460ee6a13d26e1fcc71ef1759dc44
osl1018 <osl1018@naver.com>
2024-01-23 14:50:23 +0900

    2024-01-23

---
+++ test1_1/src/main/java/com/itbank/repository/BoardDAO.java
@@ -0,0 +1,11 @@
+package com.itbank.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface BoardDAO {
+
+	@Select("select * from board2 order by idx desc fetch next 10 rows only")
+	List<BoardDTO> selectList();
+}
